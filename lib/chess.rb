66965a7810d8757afcea133e27f9f7c34311ab22
Chong Kim <chongkim@yahoo.com>
2013-12-31 13:53:51 -0500

    version 3 part 1

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,230 +1,141 @@
+require "debugger"
+class Fixnum
+  def to_sq
+    (('a'.ord + self%8).chr + ('8'.ord - self/8).chr).to_sym
+  end
+end
+
 module ChessHelper
-  'a'.upto('h').each.with_index do |file,f|
-    8.downto(1).each.with_index do |rank,r|
-      define_method("#{file}#{rank}") { f + r*8 }
+  INDICES = [*0..63]
+  INDICES.each { |idx| define_method(idx.to_sq) { idx } }
+  def white(p,w,b)
+    case p
+    when :white, :R, :N, :B, :Q, :K, :P then w
+    when :black, :r, :n, :b, :q, :k, :p then b
     end
-  end
-  def white(piece,w,b)
-    case piece
-    when /^[A-Z]$/, :white then w
-    when /^[a-z]$/, :black then b
-    end
-  end
-  def to_sq(idx)
-    y, x = idx.divmod(8)
-    ((x + 'a'.ord).chr + ('8'.ord - y).chr).to_sym
   end
   def xydiff(from, to)
     [to%8 - from%8, to/8 - from/8]
   end
-  def color(piece)
-    case piece
-    when /^[A-Z]$/ then :white
-    when /^[a-z]$/ then :black
+end
+
+class Symbol
+  def valid_move?(from, to)
+    dx, dy = xydiff(from, to)
+    case self
+    when :R, :r then dx == 0 || dy == 0
+    when :N, :n then [dx.abs, dy.abs].sort == [1,2]
+    when :B, :b then dx.abs == dy.abs
+    when :K, :k then [dx.abs, dy.abs].max <= 1
+    when :P, :p then (dx == 0     && dy == white(self,-1,1) ||
+                      dx == 0     && dy == white(self,-2,2) && from/8 == white(self,6,1) ||
+                      dx.abs == 1 && dy == white(self,-1,1)
+                     )
+
     end
   end
-  def to_col(str)
-    return nil if str.nil?
-    str[0].ord - 'a'.ord
+  def color
+    @color ||= case self
+               when :R, :N, :B, :Q, :K, :P then :white
+               when :r, :n, :b, :q, :k, :p then :black
+               else :none
+               end
   end
-  def to_row(str)
-    return nil if str.nil?
-    '8'.ord - str[-1].ord
+  def other_color
+    color == :white ? :black : color == :black ? :white : :none
+  end
+  def pawn?
+    self == :P || self == :p
   end
 end
 
 class Position
-  attr_accessor :board, :turn, :castling, :ep, :halfmove, :fullmove
-  INDICES = [*0..63]
+  include ChessHelper
+  attr_accessor :turn, :castling, :ep, :halfmove, :fullmove
   def initialize(opts={})
-    @board = opts[:board] || %w(r n b q k b n r
-                                p p p p p p p p
-                                - - - - - - - -
-                                - - - - - - - -
-                                - - - - - - - -
-                                - - - - - - - -
-                                P P P P P P P P
-                                R N B Q K B N R)
+    if opts.any? { |k,v| k.size == 1 }
+      @board = [:-]*64
+      @pieces = Hash[%w(r n b q k p R N B Q K P).map { |c| [c.to_sym, Set[]] }]
+      @pieces[:-] = Set[*0..63]
+      opts.each do |p,v|
+        next if p.size != 1
+        v = [v] unless Array === v
+        v.each do |idx|
+          self[idx] = p
+        end
+      end
+    else
+      @pieces = {}
+      @board = %w(r n b q k b n r
+                  p p p p p p p p
+                  - - - - - - - -
+                  - - - - - - - -
+                  - - - - - - - -
+                  - - - - - - - -
+                  P P P P P P P P
+                  R N B Q K B N R).map(&:to_sym).each.with_index do |p,idx|
+                    @pieces[p] ||= Set[]
+                    @pieces[p].add(idx)
+                  end
+    end
     @turn = opts[:turn] || :white
-    @castling = opts[:castling] || %w(K Q k q)
+    @castling = opts[:castling] || [:K, :Q, :k, :q]
     @ep = opts[:ep]
     @halfmove = opts[:halfmove] || 0
     @fullmove = opts[:fullmove] || 1
   end
-  def initialize_copy(other)
-    instance_variables.all? { |var|
-      value = other.instance_variable_get(var)
-      value = value.dup if value.is_a?(Array)
-      self.instance_variable_set(var, value)
-    }
+  INDICES.each { |idx| define_method(idx.to_sq) { @board[idx] } }
+  def self.[](opts)
+    Position.new(opts)
   end
-  def self.[](str,opts={})
-    position = Position.new(opts.merge(:board => %w(-)*64))
-    fn = :upcase
-    str.split.each do |s|
-      case s
-      when /^([RNBQK])?([a-h][1-8])$/ then position.board[eval $2] = ($1 || "P").send(fn)
-      when ".." then fn = :downcase
-      end
-    end
-    position
+  def []=(idx,p)
+    @pieces[@board[idx]] ||= Set[]
+    @pieces[@board[idx]].delete(idx)
+    @pieces[p] ||= Set[]
+    @pieces[p].add(idx)
+    @board[idx] = p
   end
   def inspect
     b = @board.each_slice(8).map { |row| row.join.gsub(/-+/) { |s| s.size } }.join("/")
     t = white(turn,:w,:b)
-    c = castling.empty? ? "-" : castling.join
-    e = ep ? to_sq(ep) : "-"
+    c = castling.empty? ? :- : castling.join
+    e = ep || :-
     "#{b} #{t} #{c} #{e} #{halfmove} #{fullmove}"
   end
+  def _board; @board; end
+  def _pieces; @pieces; end
   def ==(other)
-    instance_variables.all? { |var| self.instance_variable_get(var) == other.instance_variable_get(var) }
+    (self.class == other.class &&
+     @board == other._board &&
+     @pieces == other._pieces &&
+     @turn == other.turn &&
+     @castling == other.castling &&
+     @ep == other.ep &&
+     @halfmove == other.halfmove &&
+     @fullmove == other.fullmove)
+  end
+  def [](idx)
+    @board[idx]
   end
   def path_clear(from, to)
     dx, dy = xydiff(from, to)
-    return true if dx.abs != dy.abs && dx != 0 && dy != 0
+    return true if !(dx == 0 || dy == 0 || dx.abs == dy.abs)
     d = (dx <=> 0) + (dy <=> 0)*8
-    (from+d).step(to-d, d).all? { |idx| board[idx] == "-" }
-  end
-  def in_check?
-    list = INDICES.select { |idx| board[idx]==white(turn,"K","k") }
-    return false if list.empty?
-    king_idx = list[0]
-
-    INDICES.any? { |from| dup.move_piece(from, king_idx) }
-  end
-  def move_pawn(from, to)
-    piece = board[from]
-    dx, dy = xydiff(from, to)
-
-    if to == ep &&  dx.abs == 1 && dy == white(piece,-1,1) then # en passant
-      board[ep + white(piece,8,-8)] = "-"
-      return true
-    else
-      return (board[to] == '-' && dx == 0 && dy == white(piece,-1,1) || # move 1 square
-              board[to] == '-' && dx == 0 && dy == white(piece,-2,2) && from/8 == white(piece, 6, 1) || # move 2 squares
-              board[to] != '-' && dx.abs == 1 && dy == white(piece,-1,1)) # capture
-    end
-  end
-  def move_king(from, to)
-    piece = board[from]
-    dx, dy = xydiff(from, to)
-    if 1 < [dx.abs, dy.abs].max then
-      if from == white(piece, e1, e8) && to == white(piece, g1, g8) && castling.include?(white(piece,"K","k"))
-        move_piece(white(piece, h1, h8), white(piece, f1, f8))
-      elsif from == white(piece, e1, e8) && to == white(piece, c1, c8) && castling.include?(white(piece, "Q", "q"))
-        move_piece(white(piece, a1, a8), white(piece, d1, d8))
-      else
-        return false
-      end
-    end
-    castling.delete(white(piece, "K", "k"))
-    castling.delete(white(piece, "Q", "q"))
-    return true
-  end
-  def move_rook(from, to)
-    piece = board[from]
-    dx, dy = xydiff(from, to)
-
-    castling.delete(white(piece, "K", "k")) if from == h1
-    castling.delete(white(piece, "Q", "q")) if from == a1
-    return dx == 0 || dy == 0
-  end
-  def move_knight(from, to)
-    dx, dy = xydiff(from, to)
-    [dx.abs, dy.abs].sort == [1,2]
-  end
-  def move_bishop(from, to)
-    dx, dy = xydiff(from, to)
-    dx.abs == dy.abs
-  end
-  def move_queen(from , to)
-    dx, dy = xydiff(from, to)
-    dx.abs == dy.abs || dx == 0 || dy == 0
+    (from+d).step(to-d,d).all? { |idx| @board[idx] == :- }
   end
   def move_piece(from, to)
-    return nil if from == to
-
-    piece = board[from]
-    _, dy = xydiff(from, to)
-
-    return nil if !path_clear(from, to)
-    return nil if color(piece) == color(board[to])
-
-    is_valid = case piece.upcase
-               when "R" then move_rook(from, to)
-               when "N" then move_knight(from,to)
-               when "B" then move_bishop(from,to)
-               when "Q" then move_queen(from,to)
-               when "K" then move_king(from, to)
-               when "P" then move_pawn(from, to)
-               end
-    board[to] = board[from]
-    board[from] = "-"
-    @ep = piece.upcase == "P" && dy.abs == 2 ? to + white(piece,8,-8) : nil
-    is_valid && !in_check? ? self : nil
-  end
-  class IllegalMove < Exception
-    def initialize(str, position)
-      super("#{str}\n#{position}")
-    end
-  end
-  class AmbiguousMove < Exception
-    def initialize(str, position, list)
-      super("#{str}\n#{position}#{list.map { |idx| to_sq(idx) }.inspect}")
-    end
-  end
-  def to_s
-    b = @board.map { |p|
-      case p
-      when "P" then "♙"
-      when "p" then "♟"
-      when "R" then "♖"
-      when "r" then "♜"
-      when "N" then "♘"
-      when "n" then "♞"
-      when "B" then "♗"
-      when "b" then "♝"
-      when "Q" then "♕"
-      when "q" then "♛"
-      when "K" then "♔"
-      when "k" then "♚"
-      else p
+    dx, dy = xydiff(from, to)
+    piece = self[from]
+    if piece.valid_move?(from, to) && self[to].color != piece.color && path_clear(from, to) then
+      if !piece.pawn? || (dx == 0 && @board[to] == :- ||
+                          dx.abs == 1 && @board[to].color == piece.other_color ||
+                          to == ep)
+        self[ep + white(piece,8,-8)] = :- if piece.pawn? && to == ep
+        @ep = piece.pawn? && dx == 0 && dy.abs == 2 ? to + white(piece,8,-8) : nil
+        self[to] = piece
+        self[from] = :-
+        self
       end
-    }.each_slice(8).map { |row| row.join(" ") }.join("\n")
-    c = castling.empty? ? "-" : castling.join
-    e = ep ? to_sq(ep) : "-"
-    "#{b} #{turn} #{c} #{e} #{halfmove} #{fullmove}"
-  end
-  def move(str)
-    position = self.dup
-    if m = str.match(/^(?<piece>[RNBQK])?  (?<file>[a-h])?(?<rank>[1-8])?  x?  (?<sq>[a-h][1-8])  (=(?<promote>[RNBQ]))?  \+? $
-                     |^(?<castle>O-O)$
-                     |^(?<longcastle>O-O-O)$
-                     /x) then
-      to = m[:longcastle] ? white(turn,c1,c8) : m[:castle] ? white(turn,g1,g8) : eval(m[:sq])
-      piece = m[:castle] || m[:longcastle] ? white(turn, "K", "k") : (m[:piece] || "P").send(white(turn, :upcase, :downcase))
-      is_piece_taken = board[to] != '-' || piece.upcase == "P" && to == ep
-      raise IllegalMove.new(str,self) if color(piece) != turn
-
-      list = INDICES.select { |from|
-        tmp = position.dup
-        piece == tmp.board[from] && tmp.move_piece(from, to)
-      }
-      col = to_col(m[:file])
-      row = to_row(m[:rank])
-      list.select! { |from| from%8 == col } if col
-      list.select! { |from| from/8 == row } if row
-      raise IllegalMove.new(str,self) if list.empty?
-      raise AmbiguousMove.new(str,self,list) if 1 < list.size
-
-      position.move_piece(list[0], to)
-      position.board[to] = m[:promote].send(white(piece,:upcase,:downcase)) if m[:promote] && piece.upcase == "P"
-      position.turn = white(position.turn, :black, :white)
-      position.fullmove += 1 if position.turn == :white
-      position.halfmove += 1 if piece.upcase != "P" && !is_piece_taken
-    else
-      raise IllegalMove.new(str,self)
     end
-    position
   end
 end
--- spec/lib/chess_spec.rb
+++ spec/lib/chess_spec.rb
@@ -1,10 +1,25 @@
 require "chess"
-require "debugger"
 
 include ChessHelper
 
+describe Fixnum do
+  context "#to_sq" do
+    it { expect(0.to_sq).to eq :a8 }
+    it { expect(1.to_sq).to eq :b8 }
+    it { expect(8.to_sq).to eq :a7 }
+  end
+end
+
+describe Symbol do
+  context "#color" do
+    it { expect(:-.color).to eq :none }
+    it { expect(:R.color).to eq :white }
+    it { expect(:r.color).to eq :black }
+  end
+end
+
 describe ChessHelper do
-  context "coordinates" do
+  context "coords" do
     it { expect(a8).to eq 0 }
     it { expect(b8).to eq 1 }
     it { expect(a7).to eq 8 }
@@ -12,183 +27,61 @@ describe ChessHelper do
   context "white" do
     it { expect(white(:white,1,0)).to eq 1 }
     it { expect(white(:black,1,0)).to eq 0 }
-    it { expect(white(nil,1,0)).to be_nil }
-    it { expect(white("R",1,0)).to eq 1 }
-    it { expect(white("r",1,0)).to eq 0 }
-    it { expect(white("-",1,0)).to be_nil }
-  end
-  context "to_sq" do
-    it { expect(to_sq(0)).to eq :a8 }
-    it { expect(to_sq(1)).to eq :b8 }
-    it { expect(to_sq(8)).to eq :a7 }
+    it { expect(white(:R,1,0)).to eq 1 }
+    it { expect(white(:r,1,0)).to eq 0 }
+    it { expect(white(:-,1,0)).to eq nil }
   end
   context "xydiff" do
-    it { expect(xydiff(e1, g2)).to eq [2, -1] }
-  end
-  context "color" do
-    it { expect(color("R")).to eq :white }
-    it { expect(color("r")).to eq :black }
-    it { expect(color("-")).to be_nil }
-  end
-  context "to_col" do
-    it { expect(to_col("a")).to eq 0 }
-    it { expect(to_col("b")).to eq 1 }
-  end
-  context "to_row" do
-    it { expect(to_row("1")).to eq 7 }
-    it { expect(to_row("8")).to eq 0 }
+    it { expect(xydiff(a8,b3)).to eq [1,5] }
   end
 end
 
 describe Position do
-  its(:board) { should == %w(r n b q k b n r
-                             p p p p p p p p
-                             - - - - - - - -
-                             - - - - - - - -
-                             - - - - - - - -
-                             - - - - - - - -
-                             P P P P P P P P
-                             R N B Q K B N R) }
-  its(:turn) { should == :white }
-  its(:castling) { should == %w(K Q k q) }
-  its(:ep) { should be_nil }
-  its(:halfmove) { should == 0 }
-  its(:fullmove) { should == 1 }
-  context ".new(opts)" do
-    subject { Position.new(:turn => :black) }
-    its(:turn) { should == :black }
+  context ".new" do
+    its(:e4) { should == :- }
+    its(:e2) { should == :P }
+    its(:d1) { should == :Q }
+    its(:d8) { should == :q }
+    its(:turn) { should == :white }
+    its(:castling) { should == [:K, :Q, :k, :q] }
+    its(:ep) { should == nil }
+    its(:halfmove) { should == 0 }
+    its(:fullmove) { should == 1 }
   end
   context ".[]" do
-    it { expect(Position["Re4"].board[e4]).to eq "R" }
-    it { expect(Position["Re4", :turn => :black].turn).to eq :black }
+    subject { Position[R: e4, :turn => :black] }
+    its(:e4) { should == :R }
+    its(:e2) { should == :- }
+    its(:turn) { should == :black }
   end
-  context "path_clear" do
-    it { expect(Position["Re4"].path_clear(e4, e2)).to be_true }
-    it { expect(Position["Re4 Re2"].path_clear(e4, e2)).to be_true }
-    it { expect(Position["Re4 e3"].path_clear(e4, e2)).to be_false }
-    it { expect(Position["Ne4 d3 d4 d5"].path_clear(e4, c3)).to be_true }
+  context "#==" do
+    it { expect(Position[R: e4]).to eq Position[R: e4] }
   end
-  context "in_check?" do
-    it { expect(Position["Ke4 .. Re8"].in_check?).to be_true }
-    it { expect(Position["Ke4 .. Nc3"].in_check?).to be_true }
-    it { expect(Position["Re8 .. Ke4", :turn => :black].in_check?).to be_true }
-    it { expect(Position["Nc3 .. Ke4", :turn => :black].in_check?).to be_true }
-    it { expect(Position.new.in_check?).to be_false }
+  context "#path_clear" do
+    it { expect(subject.path_clear(e2,e7)).to eq true }
+    it { expect(subject.path_clear(e2,b2)).to eq false }
   end
   context "#move_piece" do
-    it { expect(Position["Ke1"].move_piece(e1,e1)).to be_nil }
-    it { expect(Position["Ke1 Rh1"].move_piece(e1, g1)).to eq Position["Kg1 Rf1", :castling => %w(k q)] }
-    it { expect(Position["Ke1 Ra1"].move_piece(e1, c1)).to eq Position["Kc1 Rd1", :castling => %w(k q)] }
-    it { expect(Position["Ke1 Rh1"].move_piece(e1, e2)).to eq Position["Ke2 Rh1", :castling => %w(k q)] }
-    it { expect(Position["Ke1 Rh1 Ra1"].move_piece(h1, h2)).to eq Position["Ke1 Rh2 Ra1", :castling => %w(Q k q)] }
-    it { expect(Position["Ke1 Rh1 Ra1"].move_piece(a1, a2)).to eq Position["Ke1 Rh1 Ra2", :castling => %w(K k q)] }
-    it { expect(Position["e5 .. f5", :ep => f6].move_piece(e5, f6)).to eq Position["f6"] }
-    it { expect(Position["Re4"].move_piece(e4, e2)).to eq Position["Re2"] }
-    it { expect(Position["Re4"].move_piece(e4, d3)).to be_nil }
-    it { expect(Position["Ne4"].move_piece(e4, c3)).to eq Position["Nc3"] }
-    it { expect(Position["Ne4"].move_piece(e4, c2)).to be_nil }
-    it { expect(Position[".. Be4"].move_piece(e4, f5)).to eq Position[".. Bf5"] }
-    it { expect(Position[".. Be4"].move_piece(e4, e2)).to be_nil }
-    it { expect(Position["Qe4"].move_piece(e4, e2)).to eq Position["Qe2"] }
-    it { expect(Position["Qe4"].move_piece(e4, d2)).to be_nil }
-    it { expect(Position["Ke4"].move_piece(e4, e3)).to eq Position["Ke3", :castling => %w(k q)] }
-    it { expect(Position["Ke4"].move_piece(e4, e2)).to be_nil }
-    it { expect(Position["e2"].move_piece(e2, e3)).to eq Position["e3"] }
-    it { expect(Position["e2"].move_piece(e2, e4)).to eq Position["e4", :ep => e3] }
-    it { expect(Position["e2"].move_piece(e2, c3)).to be_nil }
-    it { expect(Position["e4"].move_piece(e4, e3)).to be_nil }
-    it { expect(Position["e2 .. e3"].move_piece(e2, e3)).to be_nil }
-    it { expect(Position["e2 .. e3"].move_piece(e2, e4)).to be_nil }
-    it { expect(Position["e2 .. e4"].move_piece(e2, e4)).to be_nil }
-    it { expect(Position["e2 .. d3"].move_piece(e2, d3)).to eq Position["d3"] }
-    it { expect(Position["Re4 Be3"].move_piece(e4, e3)).to be_nil  }
-    it { expect(Position["Ke4 .. Rf8"].move_piece(e4, f4)).to be_nil }
-    it { expect(Position["Kf4 a2 .. Rf8"].move_piece(a2, a3)).to be_nil }
-  end
-  context "#dup" do
-    subject { Position.new }
-    let(:position) { subject.dup }
-    before(:each) { position.board[e4] = "R" }
-    it { expect(subject.board[e4]).to eq "-" }
-  end
-  context "#move" do
-    context "1. e4" do
-      subject { Position.new.move("e4") }
-      it { expect(subject).to eq Position.new(:board => %w(r n b q k b n r
-                                                           p p p p p p p p
-                                                           - - - - - - - -
-                                                           - - - - - - - -
-                                                           - - - - P - - -
-                                                           - - - - - - - -
-                                                           P P P P - P P P
-                                                           R N B Q K B N R),
-      :turn => :black, :ep => e3, :castling => %w(K Q k q), :halfmove => 0, :fullmove => 1) }
-    end
-    context "1. e4 e5" do
-      subject { Position.new.
-                move("e4").move("e5") }
-      it { expect(subject).to eq Position.new(:board => %w(r n b q k b n r
-                                                           p p p p - p p p
-                                                           - - - - - - - -
-                                                           - - - - p - - -
-                                                           - - - - P - - -
-                                                           - - - - - - - -
-                                                           P P P P - P P P
-                                                           R N B Q K B N R),
-      :turn => :white, :ep => e6, :castling => %w(K Q k q), :halfmove => 0, :fullmove => 2) }
-    end
-    context "1. e4 e5 2. Nf3" do
-      subject { Position.new.
-                move("e4").move("e5").
-                move("Nf3") }
-      it { expect(subject).to eq Position.new(:board => %w(r n b q k b n r
-                                                           p p p p - p p p
-                                                           - - - - - - - -
-                                                           - - - - p - - -
-                                                           - - - - P - - -
-                                                           - - - - - N - -
-                                                           P P P P - P P P
-                                                           R N B Q K B - R),
-      :turn => :black, :ep => nil, :castling => %w(K Q k q), :halfmove => 1, :fullmove => 2) }
-    end
-    context "1. e4 e5 2. Nf3 Nc6" do
-      subject { Position.new.
-                move("e4").move("e5").
-                move("Nf3").move("Nc6") }
-      it { expect(subject).to eq Position.new(:board => %w(r - b q k b n r
-                                                           p p p p - p p p
-                                                           - - n - - - - -
-                                                           - - - - p - - -
-                                                           - - - - P - - -
-                                                           - - - - - N - -
-                                                           P P P P - P P P
-                                                           R N B Q K B - R),
-      :turn => :white, :ep => nil, :castling => %w(K Q k q), :halfmove => 2, :fullmove => 3) }
-    end
-    context "legal moves from pgn file" do
-      it {
-        position = Position.new
-        game_number = 1
-        File.open("games/Morphy.pgn", "r") do |f|
-          while line = f.gets
-            case line
-            when /^\[/ then next
-            when %r"1-0|0-1|1/2-1/2" then
-              game_number += 1
-              position = Position.new
-              next
-            else
-              line.gsub(/\b\d+\./,"").split.each do |m|
-                expect{ position = position.move(m) }.not_to raise_error
-                puts
-                puts "Game #{game_number}"
-                puts position
-                puts m
-              end
-            end
-          end
-        end
-      }
-    end
+    it { expect(Position[R: e4].move_piece(e4,e2)).to eq Position[R: e2] }
+    it { expect(Position[R: e4].move_piece(e4,d3)).to eq nil }
+    it { expect(Position[R: e4, P: e3].move_piece(e4,e2)).to eq nil } 
+    it { expect(Position[R: e4, P: e2].move_piece(e4,e2)).to eq nil }
+    it { expect(Position[R: e4, p: e2].move_piece(e4,e2)).to eq Position[R: e2] }
+    it { expect(Position[N: e4].move_piece(e4,c3)).to eq Position[N: c3] }
+    it { expect(Position[N: e4].move_piece(e4,e5)).to eq nil }
+    it { expect(Position[N: e4, P: [d3,d4,d5]].move_piece(e4,c3)).to eq Position[N: c3, P: [d3,d4,d5]] }
+    it { expect(Position[b: e4].move_piece(e4,f5)).to eq Position[b: f5] }
+    it { expect(Position[b: e4].move_piece(e4,e5)).to eq nil }
+    it { expect(Position[K: e4].move_piece(e4,e5)).to eq Position[K: e5] }
+    it { expect(Position[K: e4].move_piece(e4,e6)).to eq nil }
+    it { expect(Position[P: e2].move_piece(e2,e3)).to eq Position[P: e3] }
+    it { expect(Position[P: e2, r: e3].move_piece(e2,e3)).to eq nil }
+    it { expect(Position[P: e2].move_piece(e2,e4)).to eq Position[P: e4, :ep => e3] }
+    it { expect(Position[P: e2, r:e4].move_piece(e2,e4)).to eq nil }
+    it { expect(Position[P: e2].move_piece(e2,d6)).to eq nil }
+    it { expect(Position[P: e2].move_piece(e2,e1)).to eq nil }
+    it { expect(Position[P: e2].move_piece(e2,d3)).to eq nil }
+    it { expect(Position[P: e2, r:d3].move_piece(e2,d3)).to eq Position[P: d3] }
+    it { expect(Position[P: e5, p:f5, :ep => f6].move_piece(e5,f6)).to eq Position[P: f6] }
   end
 end
