a0972f97a163eb635d057756c710f95ddbda586f
Chong Kim <chongkim@yahoo.com>
2014-01-06 12:22:08 -0500

    Ruby Chess v4 part 1

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,318 +1,171 @@
 require "debugger"
-class Fixnum
-  def to_sq
-    (('a'.ord + self%8).chr + ('8'.ord - self/8).chr).to_sym
-  end
-end
-class String
-  def to_idx
-    self[0].ord - 'a'.ord + ('8'.ord - self[1].ord)*8
-  end
-end
 
 module ChessHelper
-  INDICES = [*0..63]
-  INDICES.each { |idx| define_method(idx.to_sq) { idx } }
-  def white(p,w,b)
-    case p
-    when :white, :R, :N, :B, :Q, :K, :P then w
-    when :black, :r, :n, :b, :q, :k, :p then b
+  ('a'..'h').each.with_index do |col,i|
+    (1..8).each.with_index do |row,j|
+      define_method("#{col}#{row}") { i + 1 + (8 - j + 1)*10 }
     end
-  end
-  def xydiff(from, to)
-    [to%8 - from%8, to/8 - from/8]
   end
 end
 
-class Symbol
-  include ChessHelper
-  def valid_move(from, to)
-    dx, dy = xydiff(from, to)
-    case self
-    when :R, :r then dx == 0 || dy == 0
-    when :N, :n then [dx.abs, dy.abs].sort == [1,2]
-    when :B, :b then dx.abs == dy.abs
-    when :Q, :q then dx.abs == dy.abs || dx == 0 || dy == 0
-    when :K, :k then ([dx.abs, dy.abs].max <= 1 ||
-                      from == white(self,e1,e8) && (to == white(self,g1,g8) || to == white(self,c1,c8)))
-    when :P, :p then dx == 0 && (dy == white(self,-1,1) || dy == white(self,-2,2) && from/8 == white(self,6,1))
-    end
+class String
+  def to_idx
+    self[0].ord - 'a'.ord + 1 + ('8'.ord - self[1].ord + 2)*10
   end
-  def valid_capture(from, to)
-    dx, dy = xydiff(from, to)
-    case self
-    when :P, :p then dx.abs == 1 && dy == white(self,-1,1)
-    when :K, :k then [dx.abs, dy.abs].max <= 1
-    else valid_move(from, to)
-    end
-  end
-  def color
-    @color ||= case self
-               when :R, :N, :B, :Q, :K, :P then :white
-               when :r, :n, :b, :q, :k, :p then :black
-               else :none
-               end
-  end
-  def pawn?
-    self == :P || self == :p
-  end
-  def king?
-    self == :K || self == :k
+end
+class Fixnum
+  def to_sq
+    (self%10 + 'a'.ord - 1).chr + ('8'.ord - self/10 + 2).chr
   end
 end
 
 class IllegalMove < Exception
-  def initialize(str,position)
-    super("#{str}\n#{position}")
-  end
-end
-class AmbiguousMove < Exception
-  def initialize(str,position,list)
-    super("#{str}\n#{position} #{list.map(&:to_sq).inspect}")
+  def initialize(str, position, list)
+    super("#{str}\n#{position}" + (list.empty? ? "" : list.map { |from| from.to_sq }.inspect))
   end
 end
 
 class Position
-  include ChessHelper
-  attr_accessor :turn, :castling, :ep, :halfmove, :fullmove
-  def initialize(opts={})
-    if opts.any? { |k,v| k.size == 1 }
-      @board = [:-]*64
-      @pieces = Hash[%w(r n b q k p R N B Q K P).map { |c| [c.to_sym, []] }]
-      @pieces[:-] = [*0..63]
-      opts.each do |p,v|
-        next if p.size != 1
-        v = [v] unless Array === v
-        v.each do |idx|
-          self[idx] = p
-        end
-      end
-    else
-      @pieces = {}
-      @board = %w(r n b q k b n r
-                  p p p p p p p p
-                  - - - - - - - -
-                  - - - - - - - -
-                  - - - - - - - -
-                  - - - - - - - -
-                  P P P P P P P P
-                  R N B Q K B N R).map(&:to_sym).each.with_index do |p,idx|
-                    @pieces[p] ||= []
-                    @pieces[p].push(idx)
-                  end
-    end
-    @turn = opts[:turn] || :white
-    @castling = opts[:castling] || [:K, :Q, :k, :q]
-    @ep = opts[:ep]
-    @halfmove = opts[:halfmove] || 0
-    @fullmove = opts[:fullmove] || 1
+  attr_accessor :board, :turn, :castling, :ep, :halfmove, :fullmove, :king
+  def initialize
+    @board = %w(- - - - - - - - - -
+                - - - - - - - - - -
+                - r n b q k b n r -
+                - p p p p p p p p -
+                - - - - - - - - - -
+                - - - - - - - - - -
+                - - - - - - - - - -
+                - - - - - - - - - -
+                - P P P P P P P P -
+                - R N B Q K B N R -
+                - - - - - - - - - -
+                - - - - - - - - - -).map { |c| c == "-" ? nil : c }
+    @turn = :white
+    @castling = "KQkq"
+    @ep = nil
+    @halfmove = 0
+    @fullmove = 1
+    @king = { white: e1, black: e8 }
   end
   def initialize_copy(other)
-    @board = other._board.dup
-    @pieces = Hash[other._pieces.map { |k,v| [k,v.dup] }]
+    @board = other.board.dup
     @turn = other.turn
     @castling = other.castling.dup
     @ep = other.ep
     @halfmove = other.halfmove
     @fullmove = other.fullmove
-  end
-  def self.[](opts)
-    Position.new(opts)
-  end
-  def []=(idx,p)
-    @pieces[@board[idx]].delete(idx)
-    @pieces[p].push(idx)
-    @board[idx] = p
-  end
-  def inspect
-    b = @board.each_slice(8).map { |row| row.join.gsub(/-+/) { |s| s.size } }.join("/")
-    t = white(turn,:w,:b)
-    c = castling.empty? ? :- : castling.join
-    e = ep || :-
-    "#{b} #{t} #{c} #{e} #{halfmove} #{fullmove}"
-  end
-  def _board; @board; end
-  def _pieces; @pieces; end
-  def ==(other)
-    return false if self.class != other.class
-    @pieces.each { |k,v| v.sort! }
-    other._pieces.each { |k,v| v.sort! }
-    (@board == other._board &&
-     @pieces == other._pieces &&
-     @turn == other.turn &&
-     @castling == other.castling &&
-     @ep == other.ep &&
-     @halfmove == other.halfmove &&
-     @fullmove == other.fullmove)
-  end
-  def [](idx)
-    @board[idx]
-  end
-  def path_clear(from, to)
-    dx, dy = xydiff(from, to)
-    return true if !(dx == 0 || dy == 0 || dx.abs == dy.abs)
-    d = (dx <=> 0) + (dy <=> 0)*8
-    (from+d).step(to-d,d).all? { |idx| @board[idx] == :- }
-  end
-  def move_piece(from, to, act=true)
-    piece = self[from]
-    target = self[to]
-    return nil if piece.color == target.color
-    is_ep_capture = piece.pawn? && to == ep
-    is_capture = target != :- || is_ep_capture
-    if piece.king? && (to - from).abs == 2 then
-      return nil if to == white(turn,g1,g8) && !castling.include?(white(turn,:K,:k))
-      return nil if to == white(turn,c1,c8) && !castling.include?(white(turn,:Q,:q))
-      return nil if in_check? || (tmp = dup.move_piece(from, (from+to)/2)) && tmp.in_check?
-    end
-    if (!is_capture && piece.valid_move(from, to) ||
-        is_capture && piece.valid_capture(from, to)) && path_clear(from, to) then
-      @ep = piece.pawn? && (to - from).abs == 16 ? (to+from)/2 : nil if act
-      if is_ep_capture then
-        self[to%8 + from/8*8] = :- if act
-      end
-      self[to] = piece if act
-      self[from] = :- if act
-      if piece.king? && (to - from).abs == 2 then
-        f,t = from < to ? [white(turn,h1,h8), white(turn,f1,f8)] : [white(turn,a1,a8), white(turn,d1,d8)]
-        self[t] = self[f] if act
-        self[f] = :- if act
-      end
-      self
-    end
+    @king = other.king.dup
   end
   def to_s
-    b = @board.map { |p| {R:'♖',N:'♘',B:'♗',Q:'♕',K:'♔',P:'♙',r:'♜',n:'♞',b:'♝',q:'♛',k:'♚',p:'♟'}[p] || p
-    }.each_slice(8).map { |row| row.join(" ") }.join("\n")
-    c = castling.empty? ? :- : castling.join
+    b = @board.each_slice(10).to_a[2..9].map { |row| row[1..8].map { |s| s || "-" }.join(" ") }.join("\n")
+    c = castling.empty? ? :- : castling
     e = ep.nil? ? :- : ep.to_sq
     "#{b} #{turn} #{c} #{e} #{halfmove} #{fullmove}"
   end
-  def piece_list(color)
-    results = [:R, :N, :B, :Q, :K, :P]
-    results.map!(&:downcase) if color == :black
-    results
+  def white(w,b,t=turn)
+    t == :white ? w : b
+  end
+  def bounded(idx)
+    21 <= idx && idx <= 98 && idx%10 != 0 && idx%10 != 9
+  end
+  def find_repeat(piece, to, dirs, repeat)
+    list = []
+    dirs.each do |dir|
+      if repeat
+        from = to + dir
+        while bounded(from)
+          list.push(from) if board[from] == piece
+          break if board[from]
+          from += dir
+        end
+      else
+        from = to + dir
+        list.push(from) if board[from] == piece
+      end
+    end
+    list
+  end
+  def find(piece,to)
+    list = []
+    color = "RNBQKP".include?(piece) ? :white : :black
+    case piece
+    when "N", "n" then list = find_repeat(piece, to, [-21,-19,-12,-8,8,12,19,21], false)
+    when "R", "r" then list = find_repeat(piece, to, [-10,-1,1,10], true)
+    when "K", "k" then list = find_repeat(piece, to, [-11,-10,-9,-1,1,9,10,11], false)
+    when "Q", "q" then list = find_repeat(piece, to, [-11,-10,-9,-1,1,9,10,11], true)
+    when "B", "b" then list = find_repeat(piece, to, [-11,-9,9,11], true)
+    when "P", "p" then
+      if board[to] || to == ep
+        [11,9].each do |dir|
+          from = to + white(dir,-dir,color)
+          list.push(from) if board[from] == piece
+        end
+      else
+        from = to + white(10,-10,color)
+        list.push(from) if board[from] == piece
+        from = to + white(20,-20,color)
+        list.push(from) if board[from] == piece && to/10 == white(6,5,color) && board[(to+from)/2] == nil
+      end
+    end
+    list
   end
   def in_check?
-    king_idx = @pieces[white(turn,:K,:k)].first
-    piece_list(white(turn,:black,:white)).any? { |opponent|
-      @pieces[opponent].any? { |from| opponent.valid_capture(from, king_idx) && path_clear(from, king_idx) }
-    }
+    white("rnbqkp","RNBQKP").chars.any? { |piece| !find(piece, king[turn]).empty? }
   end
-  def move(*args)
-    if args.size == 1
-      str = *args
-    else
-      str = "%s-%s" % args.map { |e| e.to_sq }
-    end
+  def move(str)
     list = []
-    promote = nil
-    if m = str.match(/^(?<piece>[RNBQK])? (?<col>[a-h])?(?<row>[1-8])? x? (?<sq>[a-h][1-8]) (=(?<promote>[RNBQ]))? \+?$/x)
+    is_ep_capture = false
+    is_capture = false
+    if m = str.match(/^(?<piece>[RNBQK])? (?<col>[a-h])?(?<row>[1-8])? x?
+                     (?<sq>[a-h][1-8]) (=(?<promote>[RNBQ]))? \+?$/x) then
+      piece = m[:piece] || "P"
+      piece.downcase! if turn == :black
+      promote = m[:promote] if m[:promote]
+      promote.downcase! if promote && turn == :black
       to = m[:sq].to_idx
-      piece = (m[:piece] || "P").to_sym
-      piece = piece.downcase if turn == :black
-      list = @pieces[piece].dup
-      list.select! { |from| from%8 == m[:col].ord - 'a'.ord } if m[:col]
-      list.select! { |from| from/8 == '8'.ord - m[:row].ord } if m[:row]
-      list.select! { |from| (tmp = dup.move_piece(from, to)) && !tmp.in_check? }
-      promote = m[:promote].send(white(turn,:upcase,:downcase)).to_sym if m[:promote]
-    elsif str =~ /^O-O(-O)?$/
-      piece = white(turn, :K, :k)
-      list = [white(turn, e1, e8)]
-      to = str == "O-O" ?  white(turn, g1, g8) : white(turn, c1, c8)
-    elsif str =~ /([a-h][1-8])-([a-h][1-8])/
-      from = $1.to_idx
-      list = [from]
-      piece = self[from]
-      to = $2.to_idx
-      return nil if move_piece(from, to, false).nil? || piece.color != turn
+      col = m[:col].ord - 'a'.ord + 1 if m[:col]
+      row = '8'.ord - m[:row].ord + 2 if m[:row]
+      list = find(piece,to)
+      list.select! { |from| from%10 == col } if col
+      list.select! { |from| from/10 == row } if row
+      is_ep_capture = "Pp".include?(piece) && to == ep
+      is_capture = board[to] || is_ep_capture
+    elsif str == "O-O" && castling.include?(white("K","k"))
+      piece = white("K", "k")
+      to = white(g1,g8)
+      list.push(white(e1,e8))
+      board[white(f1,f8)] = board[white(h1,h8)]
+      board[white(h1,h8)] = nil
+      castling.delete(white("K","k"))
+    elsif str == "O-O-O" && castling.include?(white("Q","q"))
+      piece = white("K", "k")
+      to = white(c1,c8)
+      list.push(white(e1,e8))
+      board[white(d1,d8)] = board[white(a1,a8)]
+      board[white(a1,a8)] = nil
+      castling.delete(white("Q","q"))
     end
-    raise IllegalMove.new(str, self) if list.empty?
-    raise AmbiguousMove.new(str, self, list) if 1 < list.size
+    list.select! { |from|
+      tmp = dup
+      tmp.king[turn] = to if "Kk".include?(piece)
+      tmp.board[to] = tmp.board[from]
+      tmp.board[from] = nil
+      !tmp.in_check?
+    }
+    raise IllegalMove.new(str,self,list) if list.size != 1
     from = list[0]
-    if piece.pawn? || self[to] != :- then
+    @king[turn] = to if "Kk".include?(piece)
+    board[ep + white(10,-10)] = nil if is_ep_capture
+    @ep = "Pp".include?(piece) && to - from == white(-20,20) ? (to+from)/2 : nil
+    board[to] = promote || board[from]
+    board[from] = nil
+    @fullmove += 1 if turn == :black
+    if "Pp".include?(piece) || is_capture then
       @halfmove = 0
     else
       @halfmove += 1
     end
-    move_piece(from, to)
-    self[to] = promote if promote
-    @turn = white(turn, :black, :white)
-    @fullmove += 1 if turn == :white
+    @turn = white(:black, :white)
     self
-  end
-  def possible_moves(from=nil)
-    return from ? INDICES.flat_map { |to| (tmp = dup.move_piece(from, to)) && !tmp.in_check? ? [[from, to]] : [] }
-    : piece_list(turn).flat_map { |p| @pieces[p].flat_map { |piece_from| possible_moves(piece_from) } }
-  end
-  def checkmate?
-    in_check? && possible_moves.empty?
-  end
-  def stalemate?
-    !in_check? && possible_moves.empty?
-  end
-  def draw?
-    100 <= halfmove
-  end
-  def game_end?
-    checkmate? || stalemate? || draw?
-  end
-  def evaluate
-    result = 0
-    result += @pieces[:R].size*5
-    result += @pieces[:N].size*3
-    result += @pieces[:B].size*3
-    result += @pieces[:Q].size*9
-    result -= @pieces[:r].size*5
-    result -= @pieces[:n].size*3
-    result -= @pieces[:b].size*3
-    result -= @pieces[:q].size*9
-    result
-  end
-  def minimax(depth=1, alpha=-999, beta=999)
-    @@cache ||= {}
-    @@node_count ||= 0
-    cache_value = @@cache[inspect]
-    if 100 < @@node_count then
-      print "x"
-      return white(turn,-999,999)
-    end
-    @@node_count += 1
-    if cache_value then
-      print "C"
-      return cache_value
-    elsif 3 < depth then
-      print "."
-      return evaluate
-    elsif checkmate? then
-      print "#"
-      return white(turn,-100,100)
-    elsif stalemate? || draw? then
-      print "0"
-      return 0
-    else
-      if turn == :white then
-        possible_moves.each do |mv|
-          alpha = [alpha, dup.move(*mv).minimax(depth + 1, alpha, beta)].max
-          break if beta <= alpha
-        end
-        value = alpha - depth
-        @@cache[inspect] = value
-        return value
-      else
-        possible_moves.each do |mv|
-          beta = [beta, dup.move(*mv).minimax(depth + 1, alpha, beta)].min
-          break if beta <= alpha
-        end
-        value = beta + depth
-        @@cache[inspect] = value
-        return value
-      end
-    end
-  end
-  def best_move
-    @@cache = {}
-    @@node_count = 0
-    possible_moves.send(white(turn, :max_by, :min_by)) { |mv| dup.move(*mv).minimax }
   end
 end
--- spec/lib/chess_spec.rb
+++ spec/lib/chess_spec.rb
@@ -1,210 +1,118 @@
 require "chess"
+require "debugger"
 
 include ChessHelper
 
-def xcontext(*args)
-end
- 
-describe Fixnum do
-  context "#to_sq" do
-    it { expect(0.to_sq).to eq :a8 }
-    it { expect(1.to_sq).to eq :b8 }
-    it { expect(8.to_sq).to eq :a7 }
-  end
-end
-
-describe Symbol do
-  context "#color" do
-    it { expect(:-.color).to eq :none }
-    it { expect(:R.color).to eq :white }
-    it { expect(:r.color).to eq :black }
-  end
-end
-
 describe ChessHelper do
-  context "coords" do
-    it { expect(a8).to eq 0 }
-    it { expect(b8).to eq 1 }
-    it { expect(a7).to eq 8 }
+  context "coord" do
+    it { expect(a8).to eq 21 }
+    it { expect(h8).to eq 28 }
+    it { expect(a1).to eq 91 }
+    it { expect(h1).to eq 98 }
   end
-  context "white" do
-    it { expect(white(:white,1,0)).to eq 1 }
-    it { expect(white(:black,1,0)).to eq 0 }
-    it { expect(white(:R,1,0)).to eq 1 }
-    it { expect(white(:r,1,0)).to eq 0 }
-    it { expect(white(:-,1,0)).to eq nil }
+end
+
+describe String do
+  context "to_idx" do
+    it { expect("a8".to_idx).to eq 21 }
+    it { expect("h8".to_idx).to eq 28 }
+    it { expect("a1".to_idx).to eq 91 }
+    it { expect("h1".to_idx).to eq 98 }
   end
-  context "xydiff" do
-    it { expect(xydiff(a8,b3)).to eq [1,5] }
+end
+describe Fixnum do
+  context "to_sq" do
+    it { expect(21.to_sq).to eq "a8" }
+    it { expect(28.to_sq).to eq "h8" }
+    it { expect(91.to_sq).to eq "a1" }
+    it { expect(98.to_sq).to eq "h1" }
   end
 end
 
 describe Position do
   context ".new" do
-    it { expect(subject[e4]).to eq :- }
-    it { expect(subject[e2]).to eq :P }
-    it { expect(subject[d1]).to eq :Q }
-    it { expect(subject[d8]).to eq :q }
+    it { expect(subject.board[e4]).to eq nil }
+    it { expect(subject.board[e2]).to eq "P" }
+    it { expect(subject.board[d1]).to eq "Q" }
+    it { expect(subject.board[d8]).to eq "q" }
     its(:turn) { should == :white }
-    its(:castling) { should == [:K, :Q, :k, :q] }
+    its(:castling) { should == "KQkq" }
     its(:ep) { should == nil }
     its(:halfmove) { should == 0 }
     its(:fullmove) { should == 1 }
+    its(:king) { should == { white: e1, black: e8 } }
   end
-  context ".[]" do
-    subject { Position[R: e4, :turn => :black] }
-    it { expect(subject[e4]).to eq :R }
-    it { expect(subject[e2]).to eq :- }
-    its(:turn) { should == :black }
-  end
-  context "#==" do
-    it { expect(Position[R: e4]).to eq Position[R: e4] }
-  end
-  context "#path_clear" do
-    it { expect(subject.path_clear(e2,e7)).to eq true }
-    it { expect(subject.path_clear(e2,b2)).to eq false }
-  end
-  context "#dup" do
-    it {
-      position = subject.dup
-      subject[e4] = :R
-      expect(position[e4]).to eq :-
-    }
-    it {
-      position = subject.dup
-      subject.move_piece(e2,e4)
-      expect(position[e4]).to eq :-
-    }
-  end
-
-  context "#move_piece" do
-    it { expect(Position[R: e4].move_piece(e4,e2)).to eq Position[R: e2] }
-    it { expect(Position[R: e4].move_piece(e4,d3)).to eq nil }
-    it { expect(Position[R: e4, P: e3].move_piece(e4,e2)).to eq nil }
-    it { expect(Position[R: e4, P: e2].move_piece(e4,e2)).to eq nil }
-    it { expect(Position[R: e4, p: e2].move_piece(e4,e2)).to eq Position[R: e2] }
-    it { expect(Position[N: e4].move_piece(e4,c3)).to eq Position[N: c3] }
-    it { expect(Position[N: e4].move_piece(e4,e5)).to eq nil }
-    it { expect(Position[N: e4, P: [d3,d4,d5]].move_piece(e4,c3)).to eq Position[N: c3, P: [d3,d4,d5]] }
-    it { expect(Position[b: e4].move_piece(e4,f5)).to eq Position[b: f5] }
-    it { expect(Position[b: e4].move_piece(e4,e5)).to eq nil }
-    it { expect(Position[K: e4].move_piece(e4,e5)).to eq Position[K: e5] }
-    it { expect(Position[K: e4].move_piece(e4,e6)).to eq nil }
-    it { expect(Position[K: e1, R: h1, :castling => [:Q, :k, :q]].move_piece(e1,g1)).to eq nil }
-    it { expect(Position[P: e2].move_piece(e2,e3)).to eq Position[P: e3] }
-    it { expect(Position[P: e2, r: e3].move_piece(e2,e3)).to eq nil }
-    it { expect(Position[P: e2].move_piece(e2,e4)).to eq Position[P: e4, :ep => e3] }
-    it { expect(Position[P: e2, r:e4].move_piece(e2,e4)).to eq nil }
-    it { expect(Position[P: e2].move_piece(e2,d6)).to eq nil }
-    it { expect(Position[P: e2].move_piece(e2,e1)).to eq nil }
-    it { expect(Position[P: e2].move_piece(e2,d3)).to eq nil }
-    it { expect(Position[P: e2, r:d3].move_piece(e2,d3)).to eq Position[P: d3] }
-    it { expect(Position[P: e5, p:f5, :ep => f6].move_piece(e5,f6)).to eq Position[P: f6] }
-  end
-
   context "#move" do
     context "e4" do
       subject { Position.new.move("e4") }
-      it { expect(subject[e4]).to eq :P }
-      it { expect(subject[e2]).to eq :- }
+      it { expect(subject.board[e4]).to eq "P" }
+      it { expect(subject.board[e2]).to eq nil }
       its(:turn) { should == :black }
-      its(:castling) { should == [:K, :Q, :k, :q] }
+      its(:castling) { should == "KQkq" }
       its(:ep) { should == e3 }
       its(:halfmove) { should == 0 }
       its(:fullmove) { should == 1 }
     end
     context "e4 e5" do
       subject { Position.new.move("e4").move("e5") }
-      it { expect(subject[e5]).to eq :p }
-      it { expect(subject[e7]).to eq :- }
+      it { expect(subject.board[e5]).to eq "p" }
+      it { expect(subject.board[e7]).to eq nil }
       its(:turn) { should == :white }
-      its(:castling) { should == [:K, :Q, :k, :q] }
+      its(:castling) { should == "KQkq" }
       its(:ep) { should == e6 }
       its(:halfmove) { should == 0 }
       its(:fullmove) { should == 2 }
     end
     context "e4 e5 Nf3" do
       subject { Position.new.move("e4").move("e5").move("Nf3") }
-      it { expect(subject[f3]).to eq :N }
-      it { expect(subject[g1]).to eq :- }
+      it { expect(subject.board[f3]).to eq "N" }
+      it { expect(subject.board[g1]).to eq nil }
       its(:turn) { should == :black }
-      its(:castling) { should == [:K, :Q, :k, :q] }
+      its(:castling) { should == "KQkq" }
       its(:ep) { should == nil }
       its(:halfmove) { should == 1 }
       its(:fullmove) { should == 2 }
     end
     context "e4 e5 Nf3 Nf6" do
       subject { Position.new.move("e4").move("e5").move("Nf3").move("Nf6") }
-      it { expect(subject[f6]).to eq :n }
-      it { expect(subject[g8]).to eq :- }
+      it { expect(subject.board[f6]).to eq "n" }
+      it { expect(subject.board[g8]).to eq nil }
       its(:turn) { should == :white }
-      its(:castling) { should == [:K, :Q, :k, :q] }
+      its(:castling) { should == "KQkq" }
       its(:ep) { should == nil }
       its(:halfmove) { should == 2 }
       its(:fullmove) { should == 3 }
     end
     context "e4 e5 Nf3 Nf6 Nxe5" do
       subject { Position.new.move("e4").move("e5").move("Nf3").move("Nf6").move("Nxe5") }
-      it { expect(subject[e5]).to eq :N }
-      it { expect(subject[f3]).to eq :- }
+      it { expect(subject.board[e5]).to eq "N" }
+      it { expect(subject.board[f3]).to eq nil }
       its(:turn) { should == :black }
-      its(:castling) { should == [:K, :Q, :k, :q] }
+      its(:castling) { should == "KQkq" }
       its(:ep) { should == nil }
       its(:halfmove) { should == 0 }
       its(:fullmove) { should == 3 }
     end
-  end
-  context "#possible_moves" do
-    context "for a sq" do
-      subject { Position.new.possible_moves(g1) }
-      it { expect(subject).to eq [[g1, f3], [g1, h3]] }
-    end
-    context "all possible moves" do
-      subject { Position.new.possible_moves }
-      it { expect(subject).to include [e2,e4] }
-    end
-  end
-  context "#checkmate?" do
-    it { expect(Position[K: e1, k: e3].checkmate?).to eq false }
-    it { expect(Position[K: e1, k: e3, q: e2].checkmate?).to eq true }
-    it { expect(Position[k: e1, K: e3, Q: e2, :turn => :black].checkmate?).to eq true }
-  end
-  context "#checkmate?" do
-    it { expect(Position[K: e1, k: e3].stalemate?).to eq false }
-    it { expect(Position[K: e1, k: e3, p: e2].stalemate?).to eq true }
-    it { expect(Position[k: e8, K: e6, P: e7, :turn => :black].stalemate?).to eq true }
-  end
-  context "#minimax" do
-    it { expect(Position[K: e1, k: e3, q: e2].minimax).to eq (-100) }
-    it { expect(Position[k: e1, K: e3, Q: e2, :turn => :black].minimax).to eq 100 }
-    it { expect(Position[K: e1, k: e3, p: e2].minimax).to eq 0 }
-    it { expect(Position[K: e1, k: e3, p: e2, :halfmove => 100].minimax).to eq 0 }
-    it { expect(Position[K: e1, k: e3, q: d3, :turn => :black].minimax).to eq (-99) }
-    it { expect(Position[k: e1, K: e3, Q: d3, :turn => :white].minimax).to eq 99 }
-  end
-  context "#best_move" do
-    it { expect(Position[K: e1, k: e3, q: d3, :turn => :black].best_move).to eq [d3, e2] }
-    it { expect(Position[k: e1, K: e3, Q: d3, :turn => :white].best_move).to eq [d3, e2] }
-  end
-  xcontext "reads from pgn file" do
-    it {
-      File.open("games/Morphy.pgn", "r") do |f|
-        position = Position.new
-        game = 1
-        while line = f.gets
-          next if line.start_with?("[")
-          line.gsub(/\b\d+\./,"").split.each do |m|
-            case m
-            when %r"^1-0$|^0-1$|^1/2-1/2$|\*"
-              position = Position.new
-              game += 1
-            else
-              position.move(m)
-              # puts "", "Game #{game}", position, m
+    context "read moves from pgn" do
+      it {
+        File.open("games/Morphy.pgn", "r") do |f|
+          position = Position.new
+          game = 1
+          while line = f.gets
+            next if line.start_with?("[")
+            line.gsub(/\b\d+\./,"").split.each do |str|
+              case str
+              when %r"^1-0$|^0-1$|^1/2-1/2$|^\*$" then
+                position = Position.new
+                game += 1
+              else
+                position.move(str)
+                # puts "", "Game #{game}", position, str
+              end
             end
           end
         end
-      end
-    }
+      }
+    end
   end
 end
